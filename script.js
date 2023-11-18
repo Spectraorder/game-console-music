e3a100d6660da3a56ac855ce77ab0c00c025220f
Daniel <2421036333@qq.com>
2023-11-17 19:37:55 -0500

    directory name

--- script.js
+++ script.js
@@ -14,15 +14,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
             if (opacity === 1) {
                 section.style.transform = "translateY(0)";
-                bgm.src = `bgm/console${index + 1}.mp3`;
+                bgm.src = `bgm/game${formatNumber(index + 1)}.mp3`;
                 bgm.play();
-
-                // Show audio controls when section is fully visible
                 bgm.classList.remove("hide-controls");
             } else {
                 bgm.pause();
-
-                // Hide audio controls when section is not fully visible
                 bgm.classList.add("hide-controls");
             }
         });
@@ -42,5 +38,10 @@ document.addEventListener("DOMContentLoaded", function() {
         } else {
             return 1 - ((scrollPercentage - fadeOutStart) / (fadeOutEnd - fadeOutStart));
         }
+    }
+
+    // Function to format numbers with leading zeros (e.g., 1 becomes 01)
+    function formatNumber(number) {
+        return number.toString().padStart(2, '0');
     }
 });
