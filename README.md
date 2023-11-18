80be51f05583a27ed3c1124934ab1919d6b41f3d
Daniel <2421036333@qq.com>
2023-11-17 19:32:15 -0500

    test fade out

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Super Mario History index Reference
+# Super Mario History Index Reference
 
-Game01: Super Mario 64
+Game01: Super Mario Bros. (1985)
 
 Game02: Super Mario 64
--- script.js
+++ script.js
@@ -9,43 +9,37 @@ document.addEventListener("DOMContentLoaded", function() {
             const sectionTop = section.offsetTop - 100;
             const sectionBottom = sectionTop + section.offsetHeight;
 
-            // Calculate the opacity based on the scroll position
             const opacity = calculateOpacity(currentPosition, sectionTop, sectionBottom);
-
-            // Update the opacity of the section
             section.style.opacity = opacity;
 
             if (opacity === 1) {
-                // If the section is fully visible, update the transform
                 section.style.transform = "translateY(0)";
+                bgm.src = `bgm/console${index + 1}.mp3`;
+                bgm.play();
 
-                // Play BGM only if it's not already playing
-                if (bgm.paused) {
-                    bgm.src = `bgm/console${index + 1}.mp3`;
-                    bgm.play();
-                }
+                // Show audio controls when section is fully visible
+                bgm.classList.remove("hide-controls");
             } else {
-                // If the section is not fully visible, pause the BGM
                 bgm.pause();
+
+                // Hide audio controls when section is not fully visible
+                bgm.classList.add("hide-controls");
             }
         });
     });
 
-    // Function to calculate the opacity based on the scroll position
     function calculateOpacity(scrollPosition, sectionTop, sectionBottom) {
         const sectionHeight = sectionBottom - sectionTop;
         const scrollPercentage = (scrollPosition - sectionTop) / sectionHeight;
 
-        // Adjust the fade-out range as needed
         const fadeOutStart = 0.7;
         const fadeOutEnd = 0.9;
 
         if (scrollPercentage < fadeOutStart) {
-            return 1; // Section is fully visible
+            return 1;
         } else if (scrollPercentage > fadeOutEnd) {
-            return 0; // Section is fully invisible
+            return 0;
         } else {
-            // Gradually fade out the section
             return 1 - ((scrollPercentage - fadeOutStart) / (fadeOutEnd - fadeOutStart));
         }
     }
