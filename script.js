88b7315b6a363b189cbb45881bb904b7f0c25500
Daniel <2421036333@qq.com>
2023-11-17 19:15:16 -0500

    fade out scroll down

--- script.js
+++ script.js
@@ -9,12 +9,37 @@ document.addEventListener("DOMContentLoaded", function() {
             const sectionTop = section.offsetTop - 100;
             const sectionBottom = sectionTop + section.offsetHeight;
 
-            if (currentPosition >= sectionTop && currentPosition <= sectionBottom) {
-                section.style.opacity = 1;
+            // Calculate the opacity based on the scroll position
+            const opacity = calculateOpacity(currentPosition, sectionTop, sectionBottom);
+
+            // Update the opacity of the section
+            section.style.opacity = opacity;
+
+            if (opacity > 0) {
+                // If the section is still visible, update the transform and play BGM
                 section.style.transform = "translateY(0)";
                 bgm.src = `bgm/console${index + 1}.mp3`;
                 bgm.play();
             }
         });
     });
+
+    // Function to calculate the opacity based on the scroll position
+    function calculateOpacity(scrollPosition, sectionTop, sectionBottom) {
+        const sectionHeight = sectionBottom - sectionTop;
+        const scrollPercentage = (scrollPosition - sectionTop) / sectionHeight;
+
+        // Adjust the fade-out range as needed
+        const fadeOutStart = 0.7;
+        const fadeOutEnd = 0.9;
+
+        if (scrollPercentage < fadeOutStart) {
+            return 1; // Section is fully visible
+        } else if (scrollPercentage > fadeOutEnd) {
+            return 0; // Section is fully invisible
+        } else {
+            // Gradually fade out the section
+            return 1 - ((scrollPercentage - fadeOutStart) / (fadeOutEnd - fadeOutStart));
+        }
+    }
 });
