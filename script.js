004e66b5c72ee049095f1cfb284c5c594aefdde3
Daniel <2421036333@qq.com>
2023-11-17 19:11:12 -0500

    images

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     const bgm = document.getElementById("bgm");
-    const consoleSections = document.querySelectorAll(".console-section");
+    const gameSections = document.querySelectorAll(".console-section");
 
     window.addEventListener("scroll", function() {
         const currentPosition = window.scrollY;
 
-        consoleSections.forEach(function(section, index) {
+        gameSections.forEach(function(section, index) {
             const sectionTop = section.offsetTop - 100;
             const sectionBottom = sectionTop + section.offsetHeight;
 
             if (currentPosition >= sectionTop && currentPosition <= sectionBottom) {
-                // Show corresponding console content
                 section.style.opacity = 1;
                 section.style.transform = "translateY(0)";
-                
-                // Play corresponding BGM
                 bgm.src = `bgm/console${index + 1}.mp3`;
                 bgm.play();
             }
